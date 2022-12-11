3f7b59ffbfc63d1a625e113a5d22539e80212f02
AvihayFinish <119002109+AvihayFinish@users.noreply.github.com>
2022-12-11 16:20:28 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -1,31 +1,36 @@
 int isPrime(int num)
-{
+{    if (num-1==0)
+return 1;
     if(num<2)
-        return 0;
+    return 0;
     for(int i=2;i*i<=num;i++)
     {
         if(num%i==0)
-            return 0;
+        return 0;
     }
     return 1;
 }
-
-int isStrong(int num)
-{   int current_digit=0;
-    int same_name=num;
+ 
+ int isStrong(int num)
+ {   int current_digit=0;
+      int same_name=num;
     int sum=0;
+    if(num==0)
+    sum=1;
     while(num!=0)
     {
-        current_digit=num%10;
-        int sum_digit=1;
-        for(int i=1;i<=current_digit;i++)
-        {
-            sum_digit*=i;
-        }
-        sum+=sum_digit;
-        num/=10;
+     current_digit=num%10;
+     if(current_digit==0)
+     sum+=1;
+     int sum_digit=1;
+     for(int i=1;i<=current_digit;i++)
+     {
+       sum_digit*=i;
+     }
+     sum+=sum_digit;
+     num/=10;
     }
     if(sum==same_name)
-        return 1;
+    return 1;
     return 0;
-}
+ }
