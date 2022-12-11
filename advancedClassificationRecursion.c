f0b640c3faf3fb04f1c6396941d748272507de23
AvihayFinish <119002109+AvihayFinish@users.noreply.github.com>
2022-12-11 16:19:58 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,22 +1,22 @@
 int count(int num)
+{ 
+      int count=0;
+if(num==0)
 {
-    int count=0;
-    if(num==0)
-    {
-        return 1;
-    }
+    return 1;
+}
     while(num!=0)
     {
-        count++;
-        num/=10;
+    count++;
+    num/=10;
     }
-    return count;
+   return count;
 }
 
-int rev(int num,int rev_num)
+    int rev(int num,int rev_num)
 {
     if (num == 0)
-        return rev_num;
+     return rev_num; 
     rev_num = (rev_num * 10) + (num % 10);
     return rev(num / 10, rev_num);
 }
@@ -29,25 +29,27 @@ int isPalindrome(int num)
 int pow_1(int num ,int b)
 {  int sum=1;
     for(int i=1;i<=b;i++)
-        sum*=num;
+    sum*=num;
     return sum;
 
 }
 
-int isArmstrong_1(int num,int sum,int p)
-{
-
-    if(sum==0 && num==0)
-        return 1;
-    else if(sum<0|| num==0)
-        return 0;
-    int _digit= num%10;
-    num=num/10;
-    int x=pow_1(_digit,p);
+int isArmstrong_1(int num,int sum,int p)   
+{                          
+                                
+  if(sum==0 && num==0)      
+  return 1;
+  else if(sum<0|| num==0)
+  return 0;  
+  int _digit= num%10;
+   num=num/10;
+  int x=pow_1(_digit,p);
     sum=sum-x;
-    return isArmstrong_1(num,sum,p);
+  return isArmstrong_1(num,sum,p);
 }
 int isArmstrong(int num_1)
-{  int c=count(num_1);
+{
+
+  int c=count(num_1);
     return isArmstrong_1(num_1,num_1,c);
 }
